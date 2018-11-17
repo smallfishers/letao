47dab40f4b12aa0822d87285cdc0ea9c51a73a7c
smallfishers <272822385@qq.com>
2018-11-17 23:55:05 +0800

    一级分类渲染完成

---
+++ public/back/js/first.js
@@ -0,0 +1,39 @@
+$(function(){
+  var currentPage = 1;
+  var pageSize =5;
+  render();
+  function render(){
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        // 结合模板引擎渲染
+        var htmlStr = template("firstTpl", info );
+        $('tbody').html( htmlStr );
+
+        // 分页初始化
+        $('#paginator').bootstrapPaginator({
+          // 版本号
+          bootstrapMajorVersion: 3,
+          // 总页数
+          totalPages: Math.ceil( info.total / info.size ),
+          // 当前页
+          currentPage: info.page,
+          // 给按钮添加点击事件
+          onPageClicked: function( a, b, c, page ) {
+            // 更新当前页
+            currentPage = page;
+            // 重新渲染
+            render();
+          }
+        })
+      }
+    })
+  }
+})
