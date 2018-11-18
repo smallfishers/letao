f43183ea4675a43907b459128872738bcee03e60
smallfishers <272822385@qq.com>
2018-11-18 10:17:14 +0800

    一级开发分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -35,5 +35,54 @@ $(function(){
         })
       }
     })
-  }
+  };
+
+
+  $('#addBtn').click(function(){
+    $('#addModal').modal("show");
+  });
+
+  $('#form').bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+
+    fields:{
+      categoryName:{
+        validators:{
+          notEmpty:{
+            message:"请输入一级分类"
+          }
+        }
+      }
+    }
+  });
+
+
+  $('#form').on("success.form.bv",function(e){
+    e.preventDefault();
+
+    $.ajax({
+      type:'post',
+      url:'/category/addTopCategory',
+      data:$('#form').serialize(),
+      dataType:'json',
+      success: function(info){
+        if (info.success) {
+          $('#addModal').modal("hide");
+          currentPage = 1;
+          render();
+          $("#form").data("bootstrapValidator").resetForm(true);
+        }
+      }
+    })
+
+
+  })
+
+
+
+
 })
