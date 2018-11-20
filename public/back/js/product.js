0b90b95211e620dd2713540a1c7be82579dfa62d
smallfishers <272822385@qq.com>
2018-11-20 20:25:01 +0800

    商品页面完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -53,7 +53,7 @@ $(function(){
     
   });
 
-  $(".dropdowm-menu").on("click","a",function(){
+  $(".dropdown-menu").on("click","a",function(){
     var txt = $(this).text();
     $("#dropdownText").text(txt);
 
@@ -72,8 +72,131 @@ $(function(){
       picArr.unshift( picObj );
       $("#imgBox").prepend('<img src="'+ picUrl +'" style="height: 100px" alt="">');
 
+      
+      if(picArr.length>3){
+        picArr.pop();
+        $("#imgBox img:last-of-type").remove()
+      }
+      if(picArr.length===3){
+        $("form").data("bootstrapValidator").updateStatus("picStatus", "VALID" );
+      }
+    }
+  });
+
+
+
+  $("#form").bootstrapValidator({
+    excluded: [],
+
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    fields: {
+      brandId: {
+        validators: {
+          notEmpty: {
+            message: "请选择二级分类"
+          }
+        }
+      },
+
+      proName: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品名称"
+          }
+        }
+      },
+
+      proDesc: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品描述"
+          }
+        }
+      },
+
+      num: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品库存数量"
+          },
+
+          regexp: {
+            regexp: /^[1-9]\d*$/,
+            message: '请输入非零开头的数字'
+          }
+        }
+      },
+
+      size: {
+        validators: {
+          notEmpty: {
+            message: "请输入尺码"
+          },
+// xx-xx 的格式,  xx两位数字
+          regexp: {
+            regexp: /^\d{2}-\d{2}$/,
+            message: '必须是 xx-xx 的格式,  xx两位数字'
+          }
+        }
+      },
+
+      oldPrice: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品原价"
+          }
+        }
+      },
+
+      price: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品现价"
+          }
+        }
+      },
+
+      picStatus: {
+        validators: {
+          notEmpty: {
+            message: "请上传3张图片"
+          }
+        }
+      }
 
     }
+  });
+
+  $('#form').on("success.form.bv", function( e ){
+    e.preventDefault();
+    var params = $("form").serialize();
+    params += "&picName1=" + picArr[0].picName +"&picAddr1=" + picArr[0].picAddr;
+    params += "&picName2=" + picArr[1].picName +"&picAddr2=" + picArr[1].picAddr;
+    params += "&picName3=" + picArr[2].picName +"&picAddr3=" + picArr[2].picAddr;
+
+    $.ajax({
+      type:'post',
+      url: "/product/addProduct",
+      data:params,
+      dataType:"json",
+      success:function(info){
+        if(info.success){
+          $('#addModal').modal("hide");
+
+          currentPage = 1;
+          render();
+          $("form").data("bootstrapValidator").resetForm(true);
+          $("dropdownText").text("请选择二级分类");
+          $("#imgBox img").remove();
+          picArr = [];
+        }
+      }
+    })
   })
 
 
